205dc2d4f093e9ac36c1aa4a148ad3a5ffd4abcd
Evgeniy <borovoyevgen@gmail.com>
2024-12-03 14:53:25 +0400

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Evgeniy Borovoy on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
